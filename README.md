c4b36801b0e2b948dd631f3df6d3ab6a95440715
xx <lsadfbnm@gmail.com>
2018-05-11 11:48:18 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# luyanting1" 
--- main.c
+++ main.c
@@ -3,15 +3,12 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-	int i,s;
 int main(int argc, char *argv[]) {
+	int i,s=0;
 	for(i=0;i<10;i++)
 	{
 		s=s+i;
-		
 	}
 	printf("%d",s);
 	return 0;
 }
-
-                     
